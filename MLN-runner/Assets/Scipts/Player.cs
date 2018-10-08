5b70553d831e172dfaa3b392cbbe3317d05a4349
Mattias Mucherie <mattias.mattias.m@gmail.com>
2018-10-08 22:18:49 +0200

    Update controlls with new scripts, add temp jump button and update background

--- MLN-runner/Assets/Scipts/Player.cs
+++ MLN-runner/Assets/Scipts/Player.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	public float acc_mul;
-	public float maxSpeed;
-	private Rigidbody2D rb2d;
+	// public float acc_mul;
+	// public float maxSpeed;
+	// private Rigidbody2D rb2d;
 	
+	public CharacterController2D controller;
+	float horizontalMove = 0f;
+	public float runSpeed = 40f;
+	bool jump = false;
 	// Use this for initialization
-	void Start () {
-		rb2d = GetComponent<Rigidbody2D> ();
-	}
+	// void Start () {
+	// 	   rb2d = GetComponent<Rigidbody2D> ();
+	// }
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey("d"))
-		{
-			rb2d.velocity = new Vector2(Mathf.Min(rb2d.velocity.x + acc_mul, maxSpeed), rb2d.velocity.y);
-		} else if (Input.GetKey("a"))
-		{
-			rb2d.velocity = new Vector2(Mathf.Max(rb2d.velocity.x - acc_mul, -maxSpeed),rb2d.velocity.y);
-		}
-		else {
-			rb2d.velocity = new Vector2(Mathf.Lerp(rb2d.velocity.x, 0f, 0.4f), rb2d.velocity.y);
-		}
-		Debug.Log(rb2d.velocity);
+		
+		// if (Input.GetKey("d"))
+		// {
+		// 	rb2d.velocity = new Vector2(Mathf.Min(rb2d.velocity.x + acc_mul, maxSpeed), rb2d.velocity.y);
+		// } else if (Input.GetKey("a"))
+		// {
+		// 	rb2d.velocity = new Vector2(Mathf.Max(rb2d.velocity.x - acc_mul, -maxSpeed),rb2d.velocity.y);
+		// }
+		// else {
+		// 	rb2d.velocity = new Vector2(Mathf.Lerp(rb2d.velocity.x, 0f, 0.4f), rb2d.velocity.y);
+		// }
+		// Debug.Log(rb2d.velocity);
 		// float moveHorizontal=0;
 		// if (Input.GetKey("d"))moveHorizontal+=1f;
 
@@ -37,8 +42,16 @@ public class Player : MonoBehaviour {
 		// 	Vector2 acc = new Vector2(moveHorizontal, 0);
 		// 	rb2d.AddForce(acc*acc_mul); 	
 		// 	rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed),rb2d.velocity.y);
-
 		// }
-
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+	}
+	void FixedUpdate() {
+		//Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+		jump = false;
 	}
 }
